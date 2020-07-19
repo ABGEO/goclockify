51602c6e2eea6b490cfea5b87a48f57fab87d053
Temuri Takalandze <takalandzet@gmail.com>
2020-07-19 16:29:15 +0400

    ♻️ Refactor config.

--- src/config/config.go
+++ src/config/config.go
@@ -30,47 +30,49 @@ type Config struct {
 	ClockifyApiToken string `json:"clockify_api_token"`
 }
 
-func NewConfig() (*Config, error) {
+func NewConfig() (cfg *Config, err error) {
 	if "" == FilePath {
 		FilePath = filepath.Join(filepath.Join(os.Getenv("HOME"), ".config"), "abgeo/goclockify/config")
 	}
 
-	cfg := Config{
+	cfg = &Config{
 		ClockifyApiToken: "",
 	}
 
 	file, err := os.Open(FilePath)
 	if err != nil {
-		file, err = CreateConfigFile(FilePath)
+		file, err = CreateConfigFile()
 		if err != nil {
-			return &cfg, fmt.Errorf("couldn't open the goclockify config file: (%v)", err)
+			return cfg, fmt.Errorf("couldn't open the goclockify config file: (%v)", err)
 		}
 	}
 
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-		return &cfg, fmt.Errorf("the goclockify config file isn't valid json: (%v)", err)
+	if err = json.NewDecoder(file).Decode(&cfg); err != nil {
+		return cfg, fmt.Errorf("the goclockify config file isn't valid json: (%v)", err)
 	}
 
-	return &cfg, nil
+	return cfg, nil
 }
 
-func CreateConfigFile(filepath string) (*os.File, error) {
-	filepath = fmt.Sprintf("%s/abgeo/goclockify/config", xdg.ConfigHome())
-
-	if _, err := os.Stat(filepath); os.IsNotExist(err) {
-		err := os.MkdirAll(fp.Dir(filepath), os.ModePerm)
+func CreateConfigFile() (file *os.File, err error) {
+	if _, err := os.Stat(FilePath); os.IsNotExist(err) {
+		err := os.MkdirAll(fp.Dir(FilePath), os.ModePerm)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	payload := "{\"clockify_api_token\": \"\"}"
-	err := ioutil.WriteFile(filepath, []byte(payload), 0755)
+	payload, err := json.Marshal(Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	file, err := os.Open(filepath)
+	err = ioutil.WriteFile(FilePath, payload, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err = os.Open(FilePath)
 	if err != nil {
 		return nil, err
 	}
